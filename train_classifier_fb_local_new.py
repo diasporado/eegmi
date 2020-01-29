0e03c38d48c0df130befcb2a01505a4d5757daf3
Jacob <liaojxsir@gmail.com>
2020-01-29 21:39:41 +0800

    local

--- train_classifier_fb_local_new.py
+++ train_classifier_fb_local_new.py
@@ -31,7 +31,7 @@ def layers(inputs, params=None):
     pipe = DepthwiseConv3D(kernel_size=(1,4,4), strides=(1,1,1), depth_multiplier=16, padding='valid', groups=params['n_channels'])(inputs)
     pipe = BatchNormalization()(pipe)
     pipe = LeakyReLU(alpha=0.05)(pipe)
-    pipe = Conv3D(64, (1,2,3), strides=(1,3,4), padding='valid')(pipe)
+    pipe = Conv3D(64, (1,3,4), strides=(1,1,1), padding='valid')(pipe)
     pipe = BatchNormalization()(pipe)
     pipe = LeakyReLU(alpha=0.05)(pipe)
     pipe = Reshape((pipe.shape[1].value, 64))(pipe)
