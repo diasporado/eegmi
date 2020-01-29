fb4cb1af685052deb5273b7abb49ea978f761d6a
Jacob <liaojxsir@gmail.com>
2020-01-29 21:33:58 +0800

    train parallel

--- train_classifier_fb_parallel.py
+++ train_classifier_fb_parallel.py
@@ -28,13 +28,10 @@ Training model for classification of EEG samples into motor imagery classes
 '''
 
 def layers(inputs, params=None): 
-    pipe1 = DepthwiseConv3D(kernel_size=(1,3,3), strides=(1,1,1), depth_multiplier=16, padding='valid', groups=params['n_channels'])(inputs)
+    pipe1 = DepthwiseConv3D(kernel_size=(1,4,4), strides=(1,1,1), depth_multiplier=16, padding='valid', groups=params['n_channels'])(inputs)
     pipe1 = BatchNormalization()(pipe1)
     pipe1 = LeakyReLU(alpha=0.05)(pipe1)
-    pipe1 = DepthwiseConv3D(kernel_size=(1,3,3), strides=(1,1,1), depth_multiplier=16, padding='valid', groups=params['n_channels'])(pipe1)
-    pipe1 = BatchNormalization()(pipe1)
-    pipe1 = LeakyReLU(alpha=0.05)(pipe1)
-    pipe1 = Conv3D(64, (1,2,3), strides=(1,1,1), padding='valid')(pipe1)
+    pipe1 = Conv3D(64, (1,3,4), strides=(1,1,1), padding='valid')(pipe1)
     pipe1 = BatchNormalization()(pipe1)
     pipe1 = LeakyReLU(alpha=0.05)(pipe1)
     pipe1 = Reshape((pipe1.shape[1].value, 64))(pipe1)
@@ -178,7 +175,7 @@ if __name__ == '__main__': # if this file is been run directly by Python
 
         tf.reset_default_graph()
         with tf.Session() as sess:
-            # train(X_list, y, train_indices, val_indices, i+1)
+            train(X_list, y, train_indices, val_indices, i+1)
             del(X)
             del(y)
             del(X_list)
