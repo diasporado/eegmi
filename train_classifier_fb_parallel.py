a29b956ee431fc01e177b911715f5e3541101227
Jacob <liaojxsir@gmail.com>
2020-02-01 13:17:33 +0800

    parallel

--- train_classifier_fb_parallel.py
+++ train_classifier_fb_parallel.py
@@ -20,7 +20,7 @@ import read_bci_data_fb
 folder_path = 'model_results_fb_parallel'
 use_center_loss = True
 use_contrastive_center_loss = False
-batch_size = 512
+batch_size = 64
 all_classes = ['LEFT_HAND','RIGHT_HAND','FEET','TONGUE']
 n_epoch = 500
 early_stopping = 10
