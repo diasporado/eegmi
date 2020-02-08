ae032a88d553edb09442d7d714b3288ee9b20cc7
Jacob <liaojxsir@gmail.com>
2020-02-08 16:41:40 +0800

    sc

--- train_classifier_shallow_convnet.py
+++ train_classifier_shallow_convnet.py
@@ -43,7 +43,7 @@ folder_path = 'model_results_shallow_convnet'
 batch_size = 512
 all_classes = ['LEFT_HAND','RIGHT_HAND','FEET','TONGUE']
 n_epoch = 500
-early_stopping = 5
+early_stopping = 15
 
 '''
 Training model for classification of EEG samples into motor imagery classes
