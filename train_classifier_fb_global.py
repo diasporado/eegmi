d650a890de37664776cae8b82e7f41c5d40f558f
Jacob <liaojxsir@gmail.com>
2020-02-08 16:07:20 +0800

    es

--- train_classifier_fb_global.py
+++ train_classifier_fb_global.py
@@ -22,7 +22,7 @@ folder_path = 'model_results_fb_global'
 batch_size = 512
 all_classes = ['LEFT_HAND','RIGHT_HAND','FEET','TONGUE']
 n_epoch = 500
-early_stopping = 5
+early_stopping = 15
 
 '''
 Training model for classification of EEG samples into motor imagery classes
