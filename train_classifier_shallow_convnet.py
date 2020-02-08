fa7d8adcc6bce78dca80fbfc005ba31fb532dc54
Jacob <liaojxsir@gmail.com>
2020-02-08 15:26:51 +0800

    names

--- train_classifier_shallow_convnet.py
+++ train_classifier_shallow_convnet.py
@@ -40,10 +40,10 @@ def layers(inputs):
     pipe = Reshape((pipe.shape[1].value, pipe.shape[2].value, pipe.shape[4].value, 1))(pipe)
     pipe = Conv3D(40, (1,22,40), strides=(1,1,1))(pipe)
     pipe = BatchNormalization(momentum=0.1)(pipe)
-    pipe = Activation('square')(pipe)
+    pipe = Activation('square', name='square')(pipe)
     pipe = Reshape((pipe.shape[1].value, 40))(pipe)
     pipe = AveragePooling1D(pool_size=(75), strides=(15))(pipe)
-    pipe = Activation('log')(pipe)
+    pipe = Activation('log', name='log')(pipe)
     pipe = Dropout(0.5)(pipe)
     pipe = Flatten()(pipe)
     return pipe
