b7cba3c6e281e7f8e24af74bc93c889553adfa0a
diasporado <liaojxsir@gmail.com>
2020-02-24 11:02:55 +0800

    batch

--- train_classifier_fb_local_new.py
+++ train_classifier_fb_local_new.py
@@ -18,7 +18,7 @@ import read_bci_data_fb
 
 '''  Parameters '''
 folder_path = 'model_results_fb_local'
-batch_size = 512
+batch_size = 64
 all_classes = ['LEFT_HAND','RIGHT_HAND','FEET','TONGUE']
 n_epoch = 100
 early_stopping = 20
