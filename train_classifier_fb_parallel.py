90e2d7132b916b11efad0e8508fc7a0550b59d47
Jacob <liaojxsir@gmail.com>
2020-02-26 08:42:12 +0800

    p

--- train_classifier_fb_parallel.py
+++ train_classifier_fb_parallel.py
@@ -50,6 +50,7 @@ def layers(inputs, params=None):
     pipe = concatenate([pipe1,pipe2], axis=2)
     # pipe = se_block(pipe)
     pipe = Dense(64)(pipe)
+    pipe = BatchNormalization()(pipe)
     pipe = LeakyReLU(alpha=0.05)(pipe)
     pipe = Dropout(0.5)(pipe)
     pipe = Flatten()(pipe)
