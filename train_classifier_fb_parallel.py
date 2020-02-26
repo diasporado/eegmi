2b3f3669a05d6da29a2d13a4936dc6a767fed6fd
Jacob <liaojxsir@gmail.com>
2020-02-26 16:03:11 +0800

    p

--- train_classifier_fb_parallel.py
+++ train_classifier_fb_parallel.py
@@ -22,7 +22,7 @@ use_center_loss = False
 use_contrastive_center_loss = False
 batch_size = 64
 all_classes = ['LEFT_HAND','RIGHT_HAND','FEET','TONGUE']
-n_epoch = 10
+n_epoch = 20
 early_stopping = 10
 
 '''
@@ -43,28 +43,13 @@ def layers(inputs, params=None):
     pipe1 = Reshape((pipe1.shape[1].value, 64))(pipe1)
     pipe1 = AveragePooling1D(pool_size=(75), strides=(15))(pipe1)
 
-    pipe2 = Conv3D(64, (1,2,3), strides=(1,1,1), padding='valid')(inputs)
-    pipe2 = BatchNormalization()(pipe2)
-    pipe2 = LeakyReLU(alpha=0.05)(pipe2)
-    pipe2 = Conv3D(64, (1,2,2), strides=(1,1,1), padding='valid')(pipe2)
-    pipe2 = LeakyReLU(alpha=0.05)(pipe2)
-    pipe2 = Conv3D(64, (1,2,2), strides=(1,1,1), padding='valid')(pipe2)
-    pipe2 = LeakyReLU(alpha=0.05)(pipe2)
-    pipe2 = Conv3D(64, (1,2,2), strides=(1,1,1), padding='valid')(pipe2)
-    pipe2 = LeakyReLU(alpha=0.05)(pipe2)
-    pipe2 = Conv3D(64, (1,2,2), strides=(1,1,1), padding='valid')(pipe2)
-    pipe2 = BatchNormalization()(pipe2)
-    pipe2 = LeakyReLU(alpha=0.05)(pipe2)
-    pipe2 = Reshape((pipe2.shape[1].value, 64))(pipe2)
-    pipe2 = AveragePooling1D(pool_size=(75), strides=(15))(pipe2)
+    pipe3 = Conv3D(64, (1,6,7), strides=(1,1,1), padding='valid')(inputs)
+    pipe3 = BatchNormalization()(pipe3)
+    pipe3 = LeakyReLU(alpha=0.05)(pipe3)
+    pipe3 = Reshape((pipe3.shape[1].value, 64))(pipe3)
+    pipe3 = AveragePooling1D(pool_size=(75), strides=(15))(pipe3)
 
-    # pipe3 = Conv3D(64, (1,6,7), strides=(1,1,1), padding='valid')(inputs)
-    # pipe3 = BatchNormalization()(pipe3)
-    # pipe3 = LeakyReLU(alpha=0.05)(pipe3)
-    # pipe3 = Reshape((pipe3.shape[1].value, 64))(pipe3)
-    # pipe3 = AveragePooling1D(pool_size=(75), strides=(15))(pipe3)
-
-    pipe = concatenate([pipe1, pipe2], axis=2)
+    pipe = concatenate([pipe1, pipe3], axis=2)
     # pipe = se_block(pipe)
     # pipe = Dense(64)(pipe)
     # pipe = BatchNormalization()(pipe)
