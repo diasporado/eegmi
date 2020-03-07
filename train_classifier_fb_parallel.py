1ba5a643fa8605a42446073acee69d22c6748327
Jacob <liaojxsir@gmail.com>
2020-03-07 13:40:05 +0800

    a

--- train_classifier_fb_parallel.py
+++ train_classifier_fb_parallel.py
@@ -35,12 +35,14 @@ Training model for classification of EEG samples into motor imagery classes
 def layers(inputs, params=None):
     pipe1 = DepthwiseConv3D(kernel_size=(1,3,3), strides=(1,1,1), depth_multiplier=64, padding='valid', groups=params['n_channels'])(inputs)
     pipe1 = LeakyReLU(alpha=0.05)(pipe1)
-    pipe1 = DepthwiseConv3D(kernel_size=(1,3,3), strides=(1,1,1), depth_multiplier=1, padding='valid', groups=64*params['n_channels'])(pipe1)
+    pipe1 = DepthwiseConv3D(kernel_size=(1,3,3), strides=(1,1,1), depth_multiplier=1, padding='valid', groups=576)(pipe1)
     pipe1 = LeakyReLU(alpha=0.05)(pipe1)
-    pipe1 = Conv3D(64, (1,2,3), strides=(1,1,1), padding='valid')(pipe1)
+    pipe1 = DepthwiseConv3D(kernel_size=(1,2,3), strides=(1,1,1), depth_multiplier=1, padding='valid', groups=576)(pipe1)
     pipe1 = BatchNormalization()(pipe1)
     pipe1 = LeakyReLU(alpha=0.05)(pipe1)
-    pipe1 = Reshape((pipe1.shape[1].value, 64))(pipe1)
+    pipe1 = Reshape((pipe1.shape[1].value, 576))(pipe1)
+    pipe1 = Dense(64)(pipe1)
+    pipe1 = LeakyReLU(alpha=0.05)(pipe1)
     pipe1 = AveragePooling1D(pool_size=(75), strides=(15))(pipe1)
 
     pipe2 = Conv3D(256, (1,6,7), strides=(1,1,1), padding='valid')(inputs)
