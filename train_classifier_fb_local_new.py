7919e69bed1291e027500349b67cdef300c73bd8
Jacob <liaojxsir@gmail.com>
2020-03-15 23:13:44 +0800

    a

--- train_classifier_fb_local_new.py
+++ train_classifier_fb_local_new.py
@@ -114,8 +114,8 @@ def train_single_subj(X_list, y, train_indices, val_indices, subject):
     opt = optimizers.adam(lr=0.001, beta_2=0.999)
     model.compile(loss=loss, optimizer=opt, metrics=['accuracy'])
     cb = [callbacks.ProgbarLogger(count_mode='steps'),
-          callbacks.ReduceLROnPlateau(monitor='loss',factor=0.5,patience=3,min_lr=0.00001),
-          callbacks.ModelCheckpoint('./{}/A0{:d}_model.hdf5'.format(folder_path,subject),monitor='loss',verbose=0,
+          callbacks.ReduceLROnPlateau(monitor='val_loss',factor=0.5,patience=3,min_lr=0.00001),
+          callbacks.ModelCheckpoint('./{}/A0{:d}_model.hdf5'.format(folder_path,subject),monitor='val_loss',verbose=0,
                                     save_best_only=True, period=1),
           callbacks.EarlyStopping(patience=early_stopping, monitor='val_loss')]
     model.summary()
@@ -290,7 +290,7 @@ def train():
             for b in range(trials):
                 X_indices.append((a, b))
         X_indices = np.array(X_indices)
-        train_indices, val_indices = train_test_split(X_indices, test_size=0.2)
+        train_indices, val_indices = train_test_split(X_indices, test_size=0.5)
         
         tf.compat.v1.reset_default_graph()
         with tf.compat.v1.Session() as sess:
