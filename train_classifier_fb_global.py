edfa8adb5e001b6700494d077ac21ca68867eff4
diasporado <liaojxsir@gmail.com>
2020-03-17 09:05:20 +0800

    a

--- train_classifier_fb_global.py
+++ train_classifier_fb_global.py
@@ -27,7 +27,7 @@ from matplotlib import cm
 
 '''  Parameters '''
 folder_path = 'model_results_fb_global_2'
-batch_size = 256
+batch_size = 512
 n_channels = 9
 all_classes = ['LEFT_HAND','RIGHT_HAND','FEET','TONGUE']
 channel_indices = [3,8,9,10,11,12,14,15,16,17,18,19,20,22,23,24,25,26,30,31,32,38]
@@ -51,8 +51,7 @@ def layers(inputs, params=None):
     # unit = Convolution2D(1, (1,1), strides=(1,1), padding='valid')(branch_outputs[0])
     pipe = Add()(branch_outputs)
     """
-    # pipe = Conv3D(64, (25,1,1), strides=(1,1,1), padding='valid')(inputs)
-    pipe = Conv3D(64, (15,6,7), strides=(1,1,1), padding='valid')(inputs)
+    pipe = Conv3D(64, (1,6,7), strides=(1,1,1), padding='valid')(inputs)
     pipe = BatchNormalization()(pipe)
     pipe = LeakyReLU(alpha=0.05)(pipe)
     # pipe = Dropout(0.5)(pipe)
