a4d866a6a9d1fa59e9a1038c9e060791db1bc19e
diasporado <liaojxsir@gmail.com>
2020-03-17 13:13:14 +0800

    a

--- train_classifier_fb_local_new.py
+++ train_classifier_fb_local_new.py
@@ -67,7 +67,7 @@ def new_layers(inputs, params=None):
         # out = Conv3D(48, kernel_size=(75,3,3), strides=(15,1,1), padding='valid')(out)
         out = Conv3D(64, kernel_size=(1,3,3), strides=(1,1,1), padding='valid')(out)
         # out = LeakyReLU(alpha=0.05)(out)
-        out = DepthwiseConv3D(kernel_size=(1,3,3), strides=(1,1,1), padding='valid', depth_multiplier=64, groups=1)(pipe)
+        out = DepthwiseConv3D(kernel_size=(1,3,3), strides=(1,1,1), padding='valid', depth_multiplier=64, groups=1)(out)
         # out = Conv3D(64, kernel_size=(1,3,3), strides=(1,1,1), padding='valid')(out)
         # out = LeakyReLU(alpha=0.05)(out)
         out = DepthwiseConv3D(kernel_size=(1,2,3), strides=(1,1,1), padding='valid', depth_multiplier=64, groups=1)(out)
