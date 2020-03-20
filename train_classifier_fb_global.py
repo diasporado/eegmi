dd7e89fca38666946121bdef2698d0c6c16faa31
Jacob <liaojxsir@gmail.com>
2020-03-21 00:56:47 +0800

    a

--- train_classifier_fb_global.py
+++ train_classifier_fb_global.py
@@ -46,7 +46,7 @@ def layers(inputs, params=None):
         # Slicing the ith channel:
         out = Lambda(lambda x: x[:,:,:,:,i])(inputs)
         out = Lambda(lambda x: K.expand_dims(x, -1))(out)
-        out = Conv3D(64, kernel_size=(1,6,7), strides=(1,1,1), padding='valid')(out)
+        out = Conv3D(8, kernel_size=(1,6,7), strides=(1,1,1), padding='valid')(out)
         out = BatchNormalization()(out)
         out = LeakyReLU(alpha=0.05)(out)
         branch_outputs.append(out)
@@ -242,7 +242,7 @@ def train():
                     for i in range(len(subjects_train))]
 
     # Iterate training on each subject separately
-    for i in [1,3]:
+    for i in range(9):
         train_index = subj_train_order[i]
         np.random.seed(123)
         X, y, _ = read_bci_data_fb.raw_to_data(raw_edf_train[train_index], training=True, drop_rejects=True, subj=train_index)
@@ -272,7 +272,7 @@ def evaluate():
                     for i in range(len(subjects_test))]
     
     # Iterate test on each subject separately
-    for i in [1,3]:
+    for i in range(9):
         test_index = subj_test_order[i]
         X_test, y_test, _ = read_bci_data_fb.raw_to_data(raw_edf_test[test_index], training=False, drop_rejects=True, subj=test_index)
         ''' Test Model '''
@@ -355,7 +355,7 @@ def visualise_feature_maps():
 
 if __name__ == '__main__': # if this file is been run directly by Python
     
-    # train()
+    train()
     evaluate()
     # visualise()
     # visualise_feature_maps()
